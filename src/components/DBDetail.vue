19de61d6d31503db6c2de3c4c68dfc923f1cfa99
jayseryu <dgkim723@hotmail.com>
2019-04-24 18:31:15 +0900

    Infomag-v2 Frontend / 24.April.2019 - slightly looks good

--- src/components/DBDetail.vue
+++ src/components/DBDetail.vue
@@ -10,7 +10,56 @@
 
     <div class="container">
       <div class="landing_info">
-        <span>{{ landing_obj.LandingInfo.landing.name }}</span>
+
+
+        <h5>선택된 랜딩페이지</h5>
+
+        <!---->
+        <!---->
+        <div v-if="window_width > 1000" class="list_area">
+          <div class="list_header">
+            <div class="list-group-item text-center d-inline-flex justify-content-between p-1 pt-2 pb-2 text-center"
+                 style="border-radius: 0; border-bottom: 0; width:100%;">
+              <div class="col-3 p-0">업체</div>
+              <div class="col-4 p-0">페이지</div>
+              <div class="col-3 p-0">담당자</div>
+              <div class="col-1 p-0 board_centre">조회수</div>
+              <div class="col-1 p-0 board_centre">DB</div>
+            </div>
+          </div>
+          <ul class="list_body text-center list-group list-group-flush col-12 pr-0 text-center">
+            <li class="list-group-item list-group-item-action d-inline-flex justify-content-between p-1 font-weight-bold border-0">
+              <div class="col-3 p-0 col-sm-3">{{ landing_obj.LandingInfo.landing.company_name }}</div>
+              <div class="col-3 p-0 col-sm-4">{{ landing_obj.LandingInfo.landing.name }}</div>
+              <div class="col-3 p-0">{{ landing_obj.LandingInfo.landing.manager_name }}</div>
+              <div class="col-1 p-0 board_centre">{{ landing_obj.LandingInfo.landing.views }}</div>
+              <div class="col-1 p-0 board_centre">{{ landing_obj.LandingInfo.landing.collection_amount }}</div>
+            </li>
+          </ul>
+        </div>
+
+        <div v-else class="list_area">
+          <div class="list_header">
+            <div class="list-group-item text-center d-inline-flex justify-content-between p-1 pt-2 pb-2"
+                 style="border-radius: 0; border-bottom: 0; width:100%;">
+              <div class="col-2 p-0">업체</div>
+              <div class="col-5 p-0">페이지</div>
+              <div class="col-3 p-0">담당자</div>
+              <div class="col-2 p-0">DB</div>
+            </div>
+          </div>
+          <ul class="list_body text-center list-group list-group-flush col-12 pr-0">
+            <li class="list-group-item list-group-item-action d-inline-flex justify-content-between p-1 border-0">
+              <div class="col-2 p-0">{{ landing_obj.LandingInfo.landing.company_name }}</div>
+              <div class="col-5 p-0">{{ landing_obj.LandingInfo.landing.name }}</div>
+              <div class="col-3 p-0">{{ landing_obj.LandingInfo.landing.manager_name }}</div>
+              <div class="col-2 p-0">{{ landing_obj.LandingInfo.landing.collection_amount }}</div>
+            </li>
+          </ul>
+        </div>
+        <!---->
+        <!---->
+
       </div>
     </div>
 
@@ -21,13 +70,21 @@
   export default {
     name: "db_detail",
     data: () => ({
+      window_width: window.innerWidth,
       landing_id: 0,
       landing_obj: {}
     }),
-    method: {
-    },
     mounted() {
       let axios = this.$axios
+
+      // Window width calculator
+      let that = this
+      this.$nextTick(function () {
+        window.addEventListener('resize', function (e) {
+          that.window_width = window.innerWidth
+        })
+      })
+      //
       this.landing_id = this.$route.params.landing_id
       axios.get(this.$store.state.endpoints.baseUrl + 'landing/api/' + this.landing_id)
         .then((response) => {
@@ -42,6 +99,12 @@
   }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  .list_header {
+    box-shadow: 0px 0px 10px 1px rgba(0, 0, 0, 0.14);
+  }
+  .list_body {
+    margin-top: 10px;
+    border: 0;
+  }
 </style>
